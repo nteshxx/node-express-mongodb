6d2928e388169c22773f3e30e060d3d6ddbbbd48
nteshxx <nteshxx@gmail.com>
2021-05-21 20:41:35 +0530

    New Commit

--- src/app.js
+++ src/app.js
@@ -53,10 +53,6 @@ if (config.env === 'production') {
 // v1 api routes
 app.use('/v1', routes);
 
-app.get('/',(req,res) => {
-  res.send('Welcome!');
-})
-
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
